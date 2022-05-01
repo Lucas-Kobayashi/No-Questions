3476a6f3afe301da035b32fe5708ae1eb43ffeff
Lucas Kobayashi <kobacarmo2@hotmail.com>
2022-04-30 22:42:17 -0300

    Home rodando no sever

--- src/server.js
+++ src/server.js
@@ -9,9 +9,13 @@ const server = express();
 // Tornando o projeto visível para o navegador
 server.set("view engine", "ejs");
 
+// Tornando o conteúdo da pasta public visíivel
+server.use(express.static("public"));
+
 // Redirecionando o local padrão do views
 server.set("views", path.join(__dirname, "views"));
 
+// Tornando o arquivo route utilizavel para o node
 server.use(route);
 
 // Iniciando o servidor na porta 3000
