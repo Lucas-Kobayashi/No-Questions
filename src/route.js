60110d3d968c051e12934ad181a00ce29f1f1d55
Lucas Kobayashi <kobacarmo2@hotmail.com>
2022-05-02 12:16:49 -0300

    Rotas finalizadas

--- src/route.js
+++ src/route.js
@@ -6,6 +6,8 @@ const route = express.Router();
 
 // Enviando a página para o navegador
 route.get("/", (req, res) => res.render("index"));
+route.get("/sala", (req, res) => res.render("room"));
+route.get("/criar-sala", (req, res) => res.render("create-pass"));
 
 // Exportando o arquivo route.js
 module.exports = route;
