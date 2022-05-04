eb1a0526532a22a29add37e2cc1f6455e3ab802c
Lucas Kobayashi <kobacarmo2@hotmail.com>
2022-05-04 20:09:07 -0300

    Número da sala sendo exibido no front

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -6,23 +6,36 @@ module.exports = {
     const db = await Database();
     const pass = req.body.password;
     let roomId;
+    let isRoom = true;
 
-    for (var i = 0; i < 6; i++) {
-      i == 0
-        ? (roomId = Math.floor(Math.random() * 10).toString())
-        : (roomId += Math.floor(Math.random() * 10).toString());
+    while (isRoom) {
+      // Gera o número da sala
+      for (var i = 0; i < 6; i++) {
+        i == 0
+          ? (roomId = Math.floor(Math.random() * 10).toString())
+          : (roomId += Math.floor(Math.random() * 10).toString());
+      }
+      // Verifica se já existe o número da sala.
+      const roomsExistIds = await db.all(`SELECT id FROM rooms`);
+      isRoom = roomsExistIds.some(roomExistid => roomExistid === roomId);
+      if (!isRoom) {
+        // Insere o número da sala no banco de dados.
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+          ) VALUES(
+            ${parseInt(roomId)},
+            ${pass}
+          )`);
+      }
     }
-
-    await db.run(`INSERT INTO rooms (
-      id,
-      pass
-    ) VALUES(
-      ${parseInt(roomId)},
-      ${pass}
-    )`);
-
     await db.close();
 
     res.redirect(`/sala/${roomId}`);
+  },
+
+  open(req, res) {
+    const roomId = req.params.room;
+    res.render("room", { roomId: roomId });
   }
 };
--- src/route.js
+++ src/route.js
@@ -1,7 +1,7 @@
 // Importando módulos necessarios
 const express = require("express");
 const QuestionController = require("./controllers/QuestionController");
-const RoomControleler = require("./controllers/RoomController");
+const RoomController = require("./controllers/RoomController");
 
 // Iniciando a variavel route
 const route = express.Router();
@@ -11,12 +11,12 @@ route.get("/", (req, res) => res.render("index", { page: "enter-room" }));
 route.get("/criar-sala", (req, res) =>
   res.render("index", { page: "create-pass" })
 );
-route.get("/sala/:room", (req, res) => res.render("room"));
+route.get("/sala/:room", RoomController.open);
 
 // O uso de ":" significa no express que você está criando uma variável.
 // Formato que o formulário de dentro da modal tem que passar a informação.
 route.post("/question/:room/:question/:action", QuestionController.index);
-route.post("/create-room", RoomControleler.create);
+route.post("/create-room", RoomController.create);
 
 // Exportando o arquivo route.js
 module.exports = route;
